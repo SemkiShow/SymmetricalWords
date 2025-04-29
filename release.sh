4f8a24721eda3654cab63a363b21f78934340f83
SemkiShow <mgdeveloper123@gmail.com>
2025-04-29 15:12:34 +0200

    Add dataset.txt to the release

--- release.sh
+++ release.sh
@@ -9,7 +9,7 @@ g++ -o main main.cpp
 x86_64-w64-mingw32-g++ -o main.exe main.cpp
 
 # Zipping the build
-zip release.zip main main.exe libgcc_s_seh-1.dll libstdc++-6.dll LICENSE README.md
+zip release.zip main main.exe libgcc_s_seh-1.dll libstdc++-6.dll LICENSE README.md dataset.txt
 rm main main.exe
 
 # Creating a GitHub release
